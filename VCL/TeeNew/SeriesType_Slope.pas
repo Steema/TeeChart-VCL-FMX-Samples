e682637f1e7a8ca3d24f80c126df2812121d220c
david berneda <davidberneda@gmail.com>
2023-04-26 13:44:26 +0200

    Improved demo a little bit and usage of OnClickPointer event

--- VCL/TeeNew/SeriesType_Slope.pas
+++ VCL/TeeNew/SeriesType_Slope.pas
@@ -9,17 +9,21 @@ uses
   {$ENDIF}
   SysUtils, Classes, Graphics, Controls, Forms, ExtCtrls, StdCtrls,
   Dialogs,
-  Base, TeeProcs, TeEngine, Chart, TeeSlopeSeries;
+  Base, TeeProcs, TeEngine, Chart, Series, TeeSlopeSeries;
 
 type
   TSlopeSeriesForm = class(TBaseForm)
     Button1: TButton;
+    Label1: TLabel;
     procedure FormCreate(Sender: TObject);
     procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
 
     Series1 : TSlopeSeries;
+
+    procedure Series1ClickPointer(Sender: TCustomSeries; ValueIndex, X,
+      Y: Integer);
   public
     { Public declarations }
   end;
@@ -47,18 +51,26 @@ begin
 
   RandSeed:=123456;
 
-  Series1.Add(25,100,'A');  // or array: Series1.Add( [25,100] , 'A')
-  Series1.Add(50,10,'B');
-  Series1.Add(15,80,'C');
-  Series1.Add(70,30,'D');
-  Series1.Add(40,10,'E');
+  Series1.Add([25,100,60],'A');
+  Series1.Add([50,10,90],'B');
+  Series1.Add([15,80,70],'C');
+  Series1.Add([70,30,110],'D');
+  Series1.Add([40,10,20],'E');
 
   Series1.AddTitles(['X','Y']);
+
+  Series1.OnClickPointer:=Series1ClickPointer;
 end;
 
 procedure TSlopeSeriesForm.Button1Click(Sender: TObject);
 begin
   Series1.FillSampleValues;
+end;
+
+procedure TSlopeSeriesForm.Series1ClickPointer(Sender: TCustomSeries;
+                       ValueIndex, X, Y: Integer);
+begin
+  Label1.Caption:='Clicked: '+IntToStr(ValueIndex);
 end;
 
 initialization
